ce4c25c161c18cb89a694e4a62c8cf0747e20da3
Fernando Cruz <fresfer@hotmail.com>
2020-10-20 03:18:57 +0000

    Added myS3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,9 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/FERNANDO-training/s3-bucket/aws"
   version = "1.15.0"
   # insert required variables here
-  bucket = "my-s3-bucket"
-  acl = "private"
-  versioning = {
-    enabled = true
-  }
+  bucket_prefix = "my-s3-bucket"
 }
